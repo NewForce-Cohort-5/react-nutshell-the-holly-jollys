c6de865cb350abd6891a82c36dced8c6d47cd3b0
nicholasvperry <nicholasvperry@gmail.com>
2021-12-14 21:03:48 -0500

    pulled main

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,16 +6,10 @@ import { TaskForm } from "./tasks/TaskForm"
 
 export const ApplicationViews = () => {
     return (
-<<<<<<< HEAD
-                        <Routes>
-                          <Route path="/"/>
-                        </Routes>
-=======
         <TasksProvider>
             <Routes>
                 <Route path="tasks/*" element={<TaskList />} />
                 <Route path="tasks/create/*" element={<TaskForm />} />
             </Routes>
         </TasksProvider>
->>>>>>> c29fe6bbd3dfead2733af182d2d740a620f38d91
     )}
